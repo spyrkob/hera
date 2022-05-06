3dcb37bd9117f50cbf880e0001e1ccccfe174351
Lin Gao <aoingl@gmail.com>
2022-05-06 20:20:05 +0800

    Remove TESTSUITE_OPTS from podman exec environment variables

--- job.sh
+++ job.sh
@@ -74,6 +74,5 @@ run_ssh "podman exec $(env_file_if_enabled) \
         -e TOOLS_MOUNT="${TOOLS_MOUNT}" \
         -e BUILD_MOLECULE_SLAVE_SSHD_PORT="${BUILD_MOLECULE_SLAVE_SSHD_PORT}" \
         -e PROJECT_DOWNSTREAM_NAME="${PROJECT_DOWNSTREAM_NAME}" \
-        -e TESTSUITE_OPTS="${TESTSUITE_OPTS}" \
         -ti ${CONTAINER_NAME} '${BUILD_SCRIPT}' ${@}" | removeColorsControlCharactersFromOutput
 exit "${PIPESTATUS[0]}"
