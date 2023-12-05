bd17cc3e494bf572d58d531f3f17740b10827648
Wildfly.org CI <wildfly-dev@lists.jboss.org>
2023-12-05 11:29:37 +0000

    [SET-654] Remove setting locale in job.sh

--- job.sh
+++ job.sh
@@ -41,7 +41,6 @@ env_file_if_enabled() {
 
 set +u
 run_ssh "podman exec $(env_file_if_enabled) \
-        -e LANG='en_US.utf8' \
         -e JOB_NAME="${JOB_NAME}" \
         -e PARENT_JOB_NAME="${PARENT_JOB_NAME}" \
         -e PARENT_JOB_BUILD_ID="${PARENT_JOB_BUILD_ID}" \
